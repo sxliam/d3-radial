90a8ca922b8683f706c09fed8c46361c48414b61
Kemal Sokolović <kemal.sokolovic@gmail.com>
2019-07-31 11:34:02 +0200

    Initialize plugin structure
    Added sample getters and setters for API parameters, and chart initialization.

--- src/radial.js
+++ src/radial.js
@@ -1,8 +1,76 @@
 function radialChart() {
   
+  let width = 960,
+      height = 500,
+      textSize = 15;
+
+  let data = null;
+
+  let canvas;
+
   function chart(selection) {
-    selection.each(function() {});
+    selection.each(function() {
+      let svg = d3.select(this)
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+      canvas = svg.append('g');
+
+      draw();
+    });
   }
+
+  // #region Chart drawing
+
+  function draw() {
+    console.log(canvas);
+  }
+
+  // #endregion
+
+  // #region Helper functions
+  // #endregion
+
+  // #region Getters and Setters
+
+  chart.width = function(value) {
+    if (!arguments.length) {
+      return width;
+    }
+    width = value;
+
+    return chart;
+  };
+
+  chart.height = function(value) {
+    if (!arguments.length) {
+      return height;
+    }
+    height = value;
+
+    return chart;
+  };
+
+  chart.textSize = function(value) {
+    if (!arguments.length) {
+      return textSize;
+    }
+    textSize = value;
+
+    return chart;
+  };
+
+  chart.data = function(value) {
+    if (!arguments.length) {
+      return data;
+    }
+    data = value;
+
+    return chart;
+  };
+
+  // #endregion
 
   return chart;
 }
